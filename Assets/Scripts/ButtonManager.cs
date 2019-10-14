a85e4f6a8dbb8a28a411381f269c1ccc486e150f
Bryce S <bsahs@unomaha.edu>
2019-10-13 21:55:05 -0500

    Added a Fully Fuctional Menu to Main Menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+	[SerializeField]
+	string defaultScene = "ArenaOne";
+
+	public GameObject play;
+	public GameObject levelSelect;
+	public GameObject credits;
+
+	public GameObject secondMenu;
+
+	void Start()
+	{
+		secondMenu.SetActive(false);
+	}
+
+    public void HandlePlayButtonOnClickEvent()
+	{
+		SceneManager.LoadScene(defaultScene);
+	}
+
+	public void HandleLevelSelectButtonOnClickEvent()
+	{
+		play.SetActive(false);
+		levelSelect.SetActive(false);
+		credits.SetActive(false);
+
+		secondMenu.SetActive(true);
+	}
+
+	public void HandleQuitButtonOnClickEvent()
+	{
+		Application.Quit();
+	}
+
+	public void HandleBackButtonOnClickEvent()
+	{
+		SceneManager.LoadScene("Main_Menu");
+	}
+
+	public void ArenaOne()
+	{
+		SceneManager.LoadScene("ArenaOne");
+	}
+
+	public void ArenaTwo()
+	{
+		SceneManager.LoadScene("ArenaTwo");
+	}
+
+	public void Sandbox()
+	{
+		SceneManager.LoadScene("Sandbox");
+	}
+}
