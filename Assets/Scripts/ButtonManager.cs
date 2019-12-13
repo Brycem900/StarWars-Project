8dfd68d0fd234b5ec63e23c326b1a80ab9bc9fbd
Collin Moon <mooncollin@gmail.com>
2019-12-12 18:02:29 -0600

    Added player selection

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -10,13 +10,24 @@ public class ButtonManager : MonoBehaviour
 
 	public GameObject play;
 	public GameObject levelSelect;
+	public GameObject playerSelect;
 	public GameObject credits;
 
 	public GameObject secondMenu;
+	public GameObject thirdMenu;
+
+	public GameObject alien;
+	public GameObject stormtrooper;
+	public GameObject jedi;
+	public GameObject droid;
+
+	public GameObject activePlayer;
 
 	void Start()
 	{
 		secondMenu.SetActive(false);
+		thirdMenu.SetActive(false);
+
 	}
 
     public void HandlePlayButtonOnClickEvent()
@@ -28,9 +39,20 @@ public class ButtonManager : MonoBehaviour
 	{
 		play.SetActive(false);
 		levelSelect.SetActive(false);
+		playerSelect.SetActive(false);
 		credits.SetActive(false);
 
 		secondMenu.SetActive(true);
+	}
+
+	public void HandlePlayerSelectButtonOnClickEvent()
+	{
+		play.SetActive(false);
+		levelSelect.SetActive(false);
+		playerSelect.SetActive(false);
+		credits.SetActive(false);
+
+		thirdMenu.SetActive(true);
 	}
 
 	public void HandleQuitButtonOnClickEvent()
@@ -40,21 +62,63 @@ public class ButtonManager : MonoBehaviour
 
 	public void HandleBackButtonOnClickEvent()
 	{
-		SceneManager.LoadScene("Main_Menu");
+		play.SetActive(true);
+		levelSelect.SetActive(true);
+		playerSelect.SetActive(true);
+		credits.SetActive(true);
+
+		secondMenu.SetActive(false);
+		thirdMenu.SetActive(false);
 	}
 
 	public void ArenaOne()
 	{
+		DontDestroyOnLoad(activePlayer);
 		SceneManager.LoadScene("ArenaOne");
 	}
 
 	public void ArenaTwo()
 	{
+		DontDestroyOnLoad(activePlayer);
 		SceneManager.LoadScene("ArenaTwo");
 	}
 
 	public void Sandbox()
 	{
+		DontDestroyOnLoad(activePlayer);
 		SceneManager.LoadScene("Sandbox");
+	}
+
+	public void alienSelect()
+	{
+		var position = activePlayer.transform.position;
+		Destroy(activePlayer);
+		activePlayer = Instantiate<GameObject>(alien);
+		activePlayer.transform.position = position;
+		
+	}
+
+	public void stormtrooperSelect()
+	{
+		var position = activePlayer.transform.position;
+		Destroy(activePlayer);
+		activePlayer = Instantiate<GameObject>(stormtrooper);
+		activePlayer.transform.position = position;
+	}
+
+	public void droidSelect()
+	{
+		var position = activePlayer.transform.position;
+		Destroy(activePlayer);
+		activePlayer = Instantiate<GameObject>(droid);
+		activePlayer.transform.position = position;
+	}
+
+	public void jediSelect()
+	{
+		var position = activePlayer.transform.position;
+		Destroy(activePlayer);
+		activePlayer = Instantiate<GameObject>(jedi);
+		activePlayer.transform.position = position;
 	}
 }
